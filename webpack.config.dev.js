0f0a24a9c3ef49c83865b8f4839e75232cf33184
skku-sungjin <skku7714@gmail.com>
2020-10-08 00:53:49 +0900

    add common config file
    - webpack-merge package installed
    - unused config lines are deleted
    - make common file to make simple for dev and prod files

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,42 +1,14 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const common = require("./webpack.config.common");
+const { merge } = require("webpack-merge");
 
-module.exports = {
-  entry: "./src/index.js",
-  target: "web",
+module.exports = merge(common, {
   mode: "development",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
   devServer: {
-    contentBase: path.join(__dirname, "public"),
+    port: 9090,
   },
-  resolve: {
-    extensions: [".js", ".jsx", ".json"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: "/node_modules",
-        use: ["babel-loader"],
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-            options: { minimize: true },
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-      filename: "index.html",
-    }),
-  ],
-};
+});
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,39 +1,11 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const common = require("./webpack.config.common");
+const { merge } = require("webpack-merge");
 
-module.exports = {
-  entry: "./src/index.js",
-  target: "web",
+module.exports = merge(common, {
   mode: "production",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].[contentHash].bundle.js",
   },
-  resolve: {
-    extensions: [".js", ".jsx", ".json"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: "/node_modules",
-        use: ["babel-loader"],
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-            options: { minimize: true },
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-      filename: "index.html",
-    }),
-  ],
-};
+});
